a48eadef15bd668e8ba49af7421f26a9915a4474
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-01-04 17:15:23 +0000

    Add files via upload

--- release.sh
+++ release.sh
@@ -2,3 +2,5 @@ clang main.c glad_gl.c -Ofast -lglfw -lm -o spaceminer
 i686-w64-mingw32-gcc main.c glad_gl.c -Ofast -Llib -lglfw3dll -lm -o spaceminer.exe
 upx spaceminer
 upx spaceminer.exe
+cp spaceminer spaceminer.AppDir/usr/bin/
+./appimagetool-x86_64.AppImage spaceminer.AppDir
